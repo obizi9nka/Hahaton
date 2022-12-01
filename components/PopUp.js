64060888f2f84ac14e5f99eda49483cd5fb61cd1
BigAnt <antropov.ssasha@yandex.ru>
2022-12-01 11:36:23 +0300

    cap

--- components/PopUp.js
+++ components/PopUp.js
@@ -1,6 +1,11 @@
 import Script from "next/script";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default function PopUp({ znachok, setZnachok }) {
+
+    function onChange(value) {
+        console.log("Captcha value:", value);
+    }
 
     return (
         <div className={znachok != null ? "modall active" : "modall"} onClick={() => {
@@ -12,14 +17,20 @@ export default function PopUp({ znachok, setZnachok }) {
         }}>
 
             <div className="znachokAlert" onClick={e => e.stopPropagation()} >
+                <ReCAPTCHA
+                    // sitekey="6Ld8s0UjAAAAAE0ldc76WOG9yz33MWUrgBp-VU1t"
+                    sitekey="6Ld_eUkjAAAAAAIUJO8yBhVSR8HW3h_YihP1kYlm"
+                    onChange={onChange}
+                />
 
-                <Script src="https://www.google.com/recaptcha/enterprise.js?render=6Ld8s0UjAAAAAE0ldc76WOG9yz33MWUrgBp-VU1t" />
+
+                {/* <Script src="https://www.google.com/recaptcha/enterprise.js?render=6Ld8s0UjAAAAAE0ldc76WOG9yz33MWUrgBp-VU1t" />
                 {
                     grecaptcha.enterprise.ready(function () {
                         grecaptcha.enterprise.execute('6Ld8s0UjAAAAAE0ldc76WOG9yz33MWUrgBp-VU1t', { action: 'login' }).then(function (token) {
                         })
                     })
-                }
+                } */}
             </div>
         </div >
     )
