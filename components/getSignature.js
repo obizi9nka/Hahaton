bf54de09d1ba393be72e059d7c64d43dbab6fe61
BigAnt <antropov.ssasha@yandex.ru>
2022-12-03 16:25:34 +0300

    e

--- components/getSignature.js
+++ components/getSignature.js
@@ -1,10 +1,10 @@
 const { ethers } = require("ethers"); // need to change
-// const PRIVATE_KEY_SIGNER = process.env.REACT_APP_PRIVATE_KEY_SIGNER
-// const NODE_URL = process.env.NODE_URL
+const PRIVATE_KEY_SIGNER = process.env.PRIVATE_KEY_SIGNER
+const NODE_URL = process.env.NODE_URL
 
-const PRIVATE_KEY_SIGNER = "bf230942dc9130d0fbfd110cec6f4228a7e03f531df6df067b2f2bae746b7239"
-const PUBLIC_KEY_SIGNER = "0x53dA845Ebe1F9668c37b9fBd9a40EC6130040834"
-const NODE_URL = "https://goerli.infura.io/v3/ae1a9e55f1824b8f86fda122aafded26"
+// const PRIVATE_KEY_SIGNER = "bf230942dc9130d0fbfd110cec6f4228a7e03f531df6df067b2f2bae746b7239"
+// const PUBLIC_KEY_SIGNER = "0x53dA845Ebe1F9668c37b9fBd9a40EC6130040834"
+// const NODE_URL = "https://goerli.infura.io/v3/ae1a9e55f1824b8f86fda122aafded26"
 
 const getSignature = async (minter, id) => {
     console.log(PRIVATE_KEY_SIGNER, NODE_URL)
